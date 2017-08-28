096b5306e3055ff436d26547a221e276db807844
junh1024 <junh1024@gmail.com>
2017-08-28 19:03:29 -0400

    Add files via upload

---
+++ reflow.py
@@ -0,0 +1,14 @@
+"""Relow"""
+
+from io import StringIO
+import pyperclip 
+
+text = pyperclip.paste() #get from clipboard
+
+text=text.replace("\r\n\r\n","$$$$")
+text=text.replace("\r\n","")
+text=text.replace("$$$$","\r\n\r\n")
+
+pyperclip.copy(text) #output to clipboard
+
+input("Press Enter to continue...")
