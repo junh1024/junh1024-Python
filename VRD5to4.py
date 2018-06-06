39ac4309eaa92498cff22e7f16f8227517086ab5
junh1024 <junh1024@gmail.com>
2018-06-06 22:19:05 +1200

    Add files via upload

--- VRD5to4.py
+++ VRD5to4.py
@@ -1,13 +1,15 @@
 import xml.etree.ElementTree as ET
+import re, sys,string
 
+content_file=sys.argv[1]
 
-template = ET.parse('05-25_20-55-02_TVNZ 2 (AAC,eng)_Taskmaster-4.Vprj')
-content  = ET.parse('05-25_20-55-02_TVNZ 2 (AAC,eng)_Taskmaster-5.Vprj')
-output   = open('out-4.vprj','w')
+template = ET.parse(r'D-Template-4.Vprj')
+content  = ET.parse(content_file)
+# output   = open('out-4.vprj','w')
 
 # ET.dump(template)
 
-template.write('out-4.vprj')
+
 
 
 # template_soup = bs4.BeautifulSoup(template,'html.parser')
@@ -19,6 +21,12 @@ template.write('out-4.vprj')
 old_cutlist= template.getroot().find("CutList") 
 old_scnlist= template.getroot().find("SceneList") 
 new_cutlist=  content.getroot().find("CutList") 
+new_scnlist=  content.getroot().find("SceneList") 
+
+old_filename=  template.getroot().find("Filename") 
+old_OSP=  template.getroot().find("OpenStreamParams") 
+old_InputFilename=  old_OSP.find("InputFilename") 
+new_filename=   content.getroot().find("Filename") 
 
 
 # ET.dump(old_cutlist)
@@ -29,8 +37,20 @@ for cut in list(old_cutlist):
 
 for cut in new_cutlist:
 	old_cutlist.insert(99,cut)
-	# old_cutlist.remove(cut)
+# old_cutlist.remove(cut)
 	
-# ET.dump(old_cutlist)
+for thing in new_scnlist:
+	old_scnlist.insert(99,thing)
+	# old_cutlist.remove(cut)
 
-print(old_scnlist)
+# for 
+# ET.dump(template)
+# print(old_filename.text)
+old_filename.text=new_filename.text
+old_InputFilename.text=new_filename.text
+# print(old_filename.text)
+
+template.write(content_file+"4.vprj")
+
+# input("ddd")
+# print(old_scnlist)
